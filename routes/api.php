da52b8b14caa2896ddd478b627bb666d822ab11f
nexus4812 <powerbatteryboy@gmail.com>
2023-11-26 11:07:22 +0900

    facadeの削除

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+use \Illuminate\Routing\Router;
+
+/** @var Router $route */
+$route = resolve(Router::class);
+
+$route->middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +11,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+use \Illuminate\Routing\Router;
+
+/** @var Router $route */
+$route = resolve(Router::class);
+
+$route->get('/', function () {
     return view('welcome');
 });
